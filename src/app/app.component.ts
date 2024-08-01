a7e2a3a5b254d6a3db6edeb15988a88f25347eac
NoorAlkhouli <nooralkhouli99@gmail.com>
2024-08-01 23:27:57 +0300

    project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  currentLang: string | undefined;
   title = 'angular-i18n';
+  constructor(
+    @Inject(LOCALE_ID) public locale: string,
+    private route: ActivatedRoute
+  ) {}
+  switchLang() {
+    location.href = this.locale === 'ar' ? '/en' : '/ar';
+  }
+
+  ngOnInit(): void {
+    // Assuming the language is set as a route parameter
+    this.route.params.subscribe((params) => {
+      this.currentLang = params['lang'] || 'en';
+    });
+  }
+
+  getImagePath(imageName: string): string {
+    return `../../../../Angular/angular-i18n/dist/angular-i18n/browser/${this.currentLang}/assets/${imageName}`;
+  }
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'en', pathMatch: 'full' },
+  { path: ':lang', component: AppComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutes {}
